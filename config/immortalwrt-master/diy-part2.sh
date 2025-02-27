fa878d6d2618c8e81ec3f9c53894fcb2f8a845d6
闲人一枚 <qgtxqj@gmail.com>
2025-02-27 15:49:26 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -26,6 +26,9 @@ echo "DISTRIB_SOURCECODE='immortalwrt'" >>package/base-files/files/etc/openwrt_r
 rm -rf package/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 #
+# Add luci-app-nikki
+git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/luci-app-nikki
+#
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
